0c8cdf36f24b6a89c787b0e831781b0b4f0e8bb8
kNhung <nhungblbp@gmail.com>
2023-03-12 09:23:38 +0700

    define color

--- Caro/header.h
+++ Caro/header.h
@@ -18,6 +18,23 @@ using namespace std;
 #define CENTER_Y 12
 #define GOAL 5
 #define MENU_SIZE 4
+#define BLACK 0
+#define BLUE 1
+#define GREEN 2
+#define AQUA 3
+#define RED 4
+#define PURPLE 5
+#define YELLOW 6
+#define WHITE 7
+#define GRAY 8
+#define LIGHT_BLUE 9
+#define LIGHT_GREEN 10
+#define LIGHT_AQUA 11
+#define LIGHT_RED 12
+#define LIGHT_PURPLE 13
+#define LIGHT_YELLOW 14
+#define BRIGHT_WHITE 15
+
 
 struct _POINT { int x, y, c; };
 struct _LIST {
