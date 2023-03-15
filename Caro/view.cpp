495e849491fb2771a67aa8f3437d23de70ab8d1a
unknown <mathanhnhi2004@gmail.com>
2023-03-15 20:51:34 +0700

    update

--- Caro/view.cpp
+++ Caro/view.cpp
@@ -53,18 +53,6 @@ void GotoXY(int x, int y) {
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-void ResetData() {
-	for (int i = 0;i < BOARD_SIZE;i++) {
-		for (int j = 0;j < BOARD_SIZE;j++) {
-			_A[i][j].x = 4 * j + LEFT + 2;
-			_A[i][j].y = 2 * i + TOP + 1;
-			_A[i][j].c = 0;
-		}
-	}
-	_TURN = true; _COMMAND = -1;
-	_X = _A[0][0].x; _Y = _A[0][0].y;
-}
-
 void Drawboard_game() {
 	system("color E1");
 	unsigned char logo[] = { 177,219,223,223,223,219,' ',177,219,220,176,177,219,' ',' ',' ',177,219,223,220,223,219,' '
